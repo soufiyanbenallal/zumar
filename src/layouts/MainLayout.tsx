148e96665013ff4b037db28901180b810f8108f3
SOUFIYAN BENALLAL <41121758+soufiyanbenallal@users.noreply.github.com>
2022-10-13 22:01:31 +0100

    feat: add layouts

--- src/layouts/MainLayout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from 'react'
-
-export default function MainLayout (): ReactElement {
-  return (
-    <div>MainLayout</div>
-  )
-}
---
+++ src/layouts/main/MainLayout.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement, ReactNode } from 'react'
+import HeaderComponent from '@components/header/HeaderComponent'
+
+export default function MainLayout({
+    children,
+}: {
+    children: ReactNode
+}): ReactElement {
+    return (
+        <main>
+            <HeaderComponent />
+            {children}
+            {/* This space for footer */}
+        </main>
+    )
+}
