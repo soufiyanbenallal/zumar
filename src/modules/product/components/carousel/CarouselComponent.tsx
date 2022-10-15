d251f17bac4e01201ff61533a5fecc08f3acd7c8
SOUFIYAN BENALLAL <41121758+soufiyanbenallal@users.noreply.github.com>
2022-10-15 17:04:37 +0100

    feat: adapt carousel

--- src/modules/product/components/carousel/CarouselComponent.tsx
+++ src/modules/product/components/carousel/CarouselComponent.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useCallback, useState } from 'react'
+import React, { ReactElement, useCallback, useMemo, useState } from 'react'
 import styles from './Carousel.module.scss'
 import { Swiper as SwiperComponent, SwiperSlide } from 'swiper/react'
 import Swipe, { Thumbs } from 'swiper'
@@ -9,34 +9,54 @@ export default function CarouselComponent({
   images,
 }: {
   className?: string
-  images: Array<string>
+  images: string[]
 }): ReactElement {
   const [thumbsSwiper, setThumbsSwiper] = useState<Swipe | null>(null)
+  const [activeIndex, setActiveIndex] = useState<number>(0)
 
   const handleThumbSwiper = useCallback((val: Swipe) => {
     setThumbsSwiper(val)
   }, [])
+
+  /**
+   * Render list of sliders for both main thumb carousel
+   *
+   * @param {string} classes - selected image
+   */
   const sliders = useCallback(
-    (classes: string): ReactElement[] =>
+    (classes: string, thumb = false): ReactElement[] =>
       images.map((url, key) => {
         return (
-          <SwiperSlide key={key} className={classes}>
+          <SwiperSlide
+            key={key}
+            className={classNames(classes, activeIndex === key && styles.active)}
+          >
             <img src={url} alt={url} loading='lazy' />
           </SwiperSlide>
         )
       }),
-    [images],
+    [activeIndex, images],
   )
+
+  /**
+   * This func used to set the index of current image
+   *
+   * @param {Swipe} val - selected image
+   */
+  const handleChange = (val: Swipe): void => {
+    setActiveIndex(val.activeIndex)
+  }
 
   return (
     <article className={classNames(className, styles.carousel)}>
       <SwiperComponent
         style={{ width: '100%' }}
-        spaceBetween={10}
-        navigation={true}
+        spaceBetween={5}
+        navigation={false}
         thumbs={{
           swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
         }}
+        onActiveIndexChange={handleChange}
         modules={[Thumbs]}
         className={styles.swipeMain}
       >
@@ -45,15 +65,14 @@ export default function CarouselComponent({
       <SwiperComponent
         style={{ width: '100%' }}
         onSwiper={handleThumbSwiper}
-        spaceBetween={10}
+        spaceBetween={20}
         slidesPerView={4}
         freeMode={true}
-        centeredSlides={true}
         watchSlidesProgress={true}
         modules={[Thumbs]}
         className={styles.swipeThumb}
       >
-        {sliders(styles.slide)}
+        {sliders(styles.slide, true)}
       </SwiperComponent>
     </article>
   )
