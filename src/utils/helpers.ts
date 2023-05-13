efeb8cf7d6ba3d4b3d74c15cd4cdf741f1e634dc
SOUFIYAN BENALLAL <41121758+soufiyanbenallal@users.noreply.github.com>
2023-05-13 19:36:23 +0100

    fixes hsaghsga

--- src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-export function moneyFormat(value: number, fixed = 2, currency = '$'): string {
-  const re = '\\d(?=(\\d{' + 3 + '})+' + (fixed > 0 ? '\\.' : '$') + ')'
-  return (
-    currency +
-    Number(value)
-      .toFixed(Math.max(0, ~~fixed))
-      .replace(new RegExp(re, 'g'), '$&,')
-  )
-}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,4 @@
-export * from './helpers'
+export function moneyFormat(value: number, fixed = 2, currency = '$'): string {
+  const re = '\\d(?=(\\d{' + 3 + '})+' + (fixed > 0 ? '\\.' : '$') + ')'
+  return currency + Number(value).toFixed(Math.max(0, ~~fixed)).replace(new RegExp(re, 'g'), '$&,')
+}
